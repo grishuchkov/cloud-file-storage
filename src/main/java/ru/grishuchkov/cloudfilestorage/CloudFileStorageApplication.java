516ee330b12d462b49f8a430453a6f009acd9cd3
grishuchkov <gri.d.a@ya.ru>
2023-11-14 20:49:17 +0300

    Initial commit

---
+++ src/main/java/ru/grishuchkov/cloudfilestorage/CloudFileStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.grishuchkov.cloudfilestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFileStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFileStorageApplication.class, args);
+	}
+
+}
