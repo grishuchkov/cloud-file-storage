d48dba5126b444e16604838f2dc781b565b3b6f3
grishuchkov <gri.d.a@ya.ru>
2023-11-20 13:21:46 +0300

    update: sql scripts for new version db

--- src/main/resources/migration/V1__create_table.sql
+++ src/main/resources/migration/V1__create_table.sql
@@ -5,13 +5,8 @@ CREATE TABLE users(
     password varchar(256) not null
 );
 
-CREATE TABLE roles(
-    id serial primary key,
-    role varchar(50) not null unique
-);
-
 CREATE TABLE user_roles(
-    user_id bigint not null references users(id),
-    role_id int not null references roles(id),
-    primary key (user_id, role_id)
-)
+    user_id bigint references users(id),
+    role varchar(50) not null,
+    primary key (user_id, role)
+);
--- src/main/resources/migration/V2__insert_test_data.sql
+++ src/main/resources/migration/V2__insert_test_data.sql
@@ -1,3 +1,2 @@
-INSERT INTO users (id, login, email, password) VALUES (1, 'test', 'test@test.com', '123');
-INSERT INTO roles (id, role) VALUES (1, 'USER');
-INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO users (id, login, email, password) VALUES (1, 'test', 'test@test.com', '$2a$10$LvRoco8rLY.bOsVikyvM9eVasI4MMbzgrueCpZ5lDpUNzD7CRAA7W');
+INSERT INTO user_roles (user_id, role) VALUES (1, 'USER_ROLE');
