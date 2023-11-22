ae9d413581145537620d20bfc7be4330a8f52d9a
grishuchkov <gri.d.a@ya.ru>
2023-11-22 14:02:14 +0300

    add: home page

--- src/main/java/ru/grishuchkov/cloudfilestorage/controller/HomeController.java
+++ src/main/java/ru/grishuchkov/cloudfilestorage/controller/HomeController.java
@@ -1,15 +1,16 @@
 package ru.grishuchkov.cloudfilestorage.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.security.Principal;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
-    public String getHome(Principal principal){
+    @GetMapping({"/", "/home"})
+    public String getHome(@AuthenticationPrincipal UserDetails userDetails) {
+
         return "home";
     }
 }
