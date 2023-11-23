c082670e2f8544a85895f85c8bf10e2519d0c29d
grishuchkov <gri.d.a@ya.ru>
2023-11-23 16:33:45 +0300

    chore refactor

--- src/main/java/ru/grishuchkov/cloudfilestorage/controller/HomeController.java
+++ src/main/java/ru/grishuchkov/cloudfilestorage/controller/HomeController.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping({"/", "/home"})
-    public String getHome(@AuthenticationPrincipal UserDetails userDetails) {
-
+    public String getHomePage() {
         return "home";
     }
 }
