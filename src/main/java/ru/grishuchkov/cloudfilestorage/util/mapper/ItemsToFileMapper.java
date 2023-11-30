c36e19c8de7982ae386c53c8a758c716df12707c
grishuchkov <gri.d.a@ya.ru>
2023-11-30 19:51:30 +0300

    add: test for mapper

--- src/main/java/ru/grishuchkov/cloudfilestorage/util/mapper/ItemsToFileMapper.java
+++ src/main/java/ru/grishuchkov/cloudfilestorage/util/mapper/ItemsToFileMapper.java
@@ -5,19 +5,23 @@ import org.springframework.stereotype.Component;
 import ru.grishuchkov.cloudfilestorage.dto.File;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @Component
-public class ItemsToFileMetadataMapper {
+public class ItemsToFileMapper {
 
     public List<File> toFile(List<Item> items) {
         List<File> files = new ArrayList<>(items.size());
 
         for (Item item : items) {
-            String allFilename = item.objectName();
-            String extension = getExtension(allFilename);
-            File file = createFileMetadata(allFilename, extension);
+            String nameWithPathAndExtension = item.objectName();
+
+            String extension = getExtension(nameWithPathAndExtension);
+            String filename = getFilename(nameWithPathAndExtension);
+
+            File file = createFile(filename, extension);
             files.add(file);
         }
         Collections.reverse(files);
@@ -32,7 +36,16 @@ public class ItemsToFileMetadataMapper {
         return filename.substring(indexOfDot + 1);
     }
 
-    private File createFileMetadata(String filename, String extension) {
+    private String getFilename(String nameWithPathAndExtension){
+        List<String> parts = Arrays.stream(nameWithPathAndExtension.split("/")).toList();
+
+        if(parts.isEmpty()){
+            return nameWithPathAndExtension;
+        }
+        return parts.get(parts.size() - 1);
+    }
+
+    private File createFile(String filename, String extension) {
         return File.builder()
                 .filename(filename)
                 .extension(extension)
--- src/test/java/ru/grishuchkov/cloudfilestorage/util/mapper/ItemsToFileMapperTest.java
+++ src/test/java/ru/grishuchkov/cloudfilestorage/util/mapper/ItemsToFileMapperTest.java
@@ -5,18 +5,38 @@ import org.junit.jupiter.api.Test;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ItemsToFileMapperTest {
-    private final ItemsToFileMetadataMapper mapper = new ItemsToFileMetadataMapper();
+    private final ItemsToFileMapper mapper = new ItemsToFileMapper();
 
     @Test
-    void name() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    void getExtension() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Method method = mapper.getClass().getDeclaredMethod("getExtension", String.class);
         method.setAccessible(true);
 
-        String filename = "logo.dsd";
+        String filename = "logo.png";
 
         String extension = (String) method.invoke(mapper, filename);
-        assertEquals(".dsd", extension);
+        assertEquals("png", extension);
+    }
+
+
+    @Test
+    void toFile() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = mapper.getClass().getDeclaredMethod("getFilename", String.class);
+        method.setAccessible(true);
+
+        String difficultPath = "hello/tmp/java/logo.psd/";
+        String simplePath = "logo.psd";
+
+        String filenameFromDifficultPath = (String) method.invoke(mapper, difficultPath);
+        String filenameFromSimplePath = (String) method.invoke(mapper, simplePath);
+
+        System.out.println(filenameFromDifficultPath);
+        System.out.println(filenameFromSimplePath);
+
+        assertEquals("logo.psd", filenameFromDifficultPath);
+        assertEquals("logo.psd", filenameFromSimplePath);
     }
 }
