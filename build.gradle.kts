dcacedda69849af6fb41e287de50bb6d19d09093
grishuchkov <gri.d.a@ya.ru>
2024-01-17 12:58:38 +0300

    add: redis session cache

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,25 +22,38 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
+
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	//security
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+
+	//data
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.flywaydb:flyway-core")
+	runtimeOnly("org.postgresql:postgresql")
+
+	//thymeleaf
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+
+	//redis
+	implementation("org.springframework.session:spring-session-data-redis")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+	//minio
 	implementation("io.minio:minio:8.5.7")
 
-	testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-	testImplementation("org.assertj:assertj-core:3.25.1")
-
-	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("org.postgresql:postgresql")
-	annotationProcessor("org.projectlombok:lombok")
-
+	//other
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
 
+	//QA
+	testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+	testImplementation("org.assertj:assertj-core:3.25.1")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
--- src/main/java/ru/grishuchkov/cloudfilestorage/config/AppConfig.java
+++ src/main/java/ru/grishuchkov/cloudfilestorage/config/AppConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableJpaRepositories("ru.grishuchkov.cloudfilestorage.repository")
+@EnableRedisHttpSession
 public class AppConfig {
 
     @Bean
